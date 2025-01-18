57104a7fa03f6fda63a7c597d7f37318a3caf5fc
Elena <e.brazhnikova@sibset-team.ru>
2025-01-18 18:35:52 +0700

    fix middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,5 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const authenticated = useCookie('user')
 
-    if (!authenticated.value) return navigateTo('/login')
+    if (!authenticated.value && to.path !== '/login') return navigateTo('/login')
+
+    if (authenticated.value && to.path === '/login') return navigateTo('/login')
 })
